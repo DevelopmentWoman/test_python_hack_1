340561ad18d30e77e75ca25eabb7c0712b4aacff
Yelitza Suniaga <87539202+DevelopmentWoman@users.noreply.github.com>
2023-10-12 11:13:54 -0400

    Update hack_7.py

--- hack_7.py
+++ hack_7.py
@@ -2,8 +2,6 @@
 loop: while [] output => [5,4,3,2,1,0]
 """
 
-#En este ejercicio utilizo list comprehension para crear una lista con una sintaxis mas corta basada en el 
-#rango que he especificado en el bucle y el método de lista reverse() para invertir los valores.
 def fn_hack_7():    
     result = []
     count = 5
